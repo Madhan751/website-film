58ae3b242b2acaa0b338ad7c2b54fd65469f94ca
madhan751 <madhanr751@gmail.com>
2023-11-06 22:45:23 -0800

    pertama

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <h2>Welcome to the Dashboard</h2>
+        <p>This is your personal dashboard page.</p>
+        <a href="logout.php" class="btn btn-danger">Logout</a>
+    </div>
+</body>
+</html>
